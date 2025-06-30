c2a4785b8c17e3c3d428e9b92729c577797baa90
Rikit0 <marumochan115@gmail.com>
2025-06-30 21:30:58 +0900

    2025/06/30 21:30

--- gekichan-app/resources/views/home.blade.php
+++ gekichan-app/resources/views/home.blade.php
@@ -3,20 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>劇ch-ホーム</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>劇ちゃん - ホーム</title>
 </head>
 <body>
-    <h1>ようこそ,劇chへ!!</h1>
-    <p>ここは劇伴好きが集まる掲示板サイトです</p>
-    <li>
-        <h3>こんな感じでスレ建ててみてください</h3>
-        <ul>劇伴について語る</ul>
-        <ul>おすすめの作品</ul>
-        <ul>今月のライブ情報</ul>
-        <ul>あの作品のこのシーンで流れてた曲は何？</ul>
-    </li>
+
+    <div class="top">
+
+        <h1>ようこそ,劇chへ!!</h1>
+
+    </div>
+    
+    <div class="body " style="text-aling: center">
+
+        <h3>ここは劇伴好きが集まる掲示板サイトです</h3>
+        <li>
+            <h3>こんな感じでスレ建ててみてください</h3>
+            <ul>劇伴について語る</ul>
+            <ul>おすすめの作品</ul>
+            <ul>今月のライブ情報</ul>
+            <ul>あの作品のこのシーンで流れてた曲は何？</ul>
+        </li>
 
     <a href="{{ url('/threads') }}">スレッド一覧表</a>
+
+    </div>
+
 
 </body>
 </html>
